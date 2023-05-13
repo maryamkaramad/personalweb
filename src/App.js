15aaffcdf4fbed1173f646f334c2dd092f7826ef
maryam <maryamkaramad73@gmail.com>
2023-05-13 15:44:26 +0330

    material rtl & Helmet & TITLE & ICON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import { Button } from '@mui/material'
+import { TextField } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/system'
+import rtlPlugin from 'stylis-plugin-rtl'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+import { prefixer } from 'stylis'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
+const theme = createTheme({
+  direction: 'rtl'
+})
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin]
+})
 function App () {
   return (
-    <div className=''>
-      <Button variant='contained'> کلیک کنید</Button>
-    </div>
+    <>
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <HelmetProvider>
+            <Helmet>
+              <title>وبسایت شخصی من</title>
+            </Helmet>
+            <div>خوش آمدید</div>
+          </HelmetProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   )
 }
 
