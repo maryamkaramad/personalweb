5a304a1629b80999cdeac48b5a3b6d3637d516a2
maryam <maryamkaramad73@gmail.com>
2023-07-26 10:21:28 +0330

    first int tabs && tab

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
-import { Avatar, Box, Divider, Hidden, Typography } from '@mui/material'
+import { Avatar, Box, Divider, Hidden, Tab, Tabs, Typography } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
+import {
+  HomeRounded,
+  FaceRounded,
+  TextSnippetRounded,
+  TerminalRounded,
+  MessageRounded,
+  ConnectWithoutContactRounded,
+  FavoriteRounded,
+  CopyrightRounded,
+} from "@mui/icons-material";
+import { grey } from '@mui/material/colors';
 const Sidebar = () => {
   return (
     <Grid2 xs={0} md={3} sx={{ backgroundColor: 'grey.main' }}>
@@ -25,6 +36,86 @@ const Sidebar = () => {
         <Typography variant='h6'>برنامه نویس فرانت</Typography>
       </Box>
       <Divider variant='middle' />
+      {/* <Tabs variant='scrollable' orientation="vertical" scrollButtons="auto">
+        <Tab label="خانه" icon={<HomeRoundedIcon />} iconPosition="start">
+        </Tab>
+      </Tabs> */}
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        scrollButton="auto"
+        allowScrollButtonsMobile
+
+
+      >
+        <Tab
+          label="صفحه اصلی"
+          icon={<HomeRounded />}
+          iconPosition="start"
+
+        />
+
+        <Tab
+          label="درباره من"
+          icon={<FaceRounded />}
+          iconPosition="start"
+        />
+        <Tab
+          label="رزومه من"
+          icon={<TextSnippetRounded />}
+          iconPosition="start"
+        />
+        <Tab
+          label="نمونه کارها"
+          icon={<TerminalRounded />}
+          iconPosition="start"
+        />
+        <Tab
+          label="نظرات دانشجویان"
+          icon={<MessageRounded />}
+          iconPosition="start"
+        />
+        <Tab
+          label="ارتباط با من"
+          icon={<ConnectWithoutContactRounded />}
+          iconPosition="start"
+        />
+      </Tabs>
+
+      <Divider variant="middle" color={grey.main} sx={{ mt: 2 }} />
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "end",
+          alignItems: "center",
+          height: 100,
+        }}
+      >
+        <Typography variant="subtitle2" color="whitesmoke">
+          طراحی شده با{" "}
+          <FavoriteRounded
+            sx={{
+              verticalAlign: "middle",
+              color: "tomato",
+              height: 20,
+            }}
+          />
+        </Typography>
+
+        <Typography
+          variant="caption"
+          color="whitesmoke"
+          sx={{ mt: 2 }}
+        >
+          کپی رایت ۱۴۰۱{" "}
+          <CopyrightRounded
+            sx={{ verticalAlign: "middle", height: 16 }}
+          />
+        </Typography>
+      </Box>
     </Grid2>
   )
 }
